2902bbb348b12e3e1107539d26222ab3b8bf04f4
Naoto891 <pedromurillo0809@gmail.com>
2023-08-30 13:56:46 -0500

    20200016_TextoComposicion

---
+++ app/src/main/java/com/example/codelab1/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.example.codelab1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.example.codelab1.ui.theme.Codelab1Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Codelab1Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting(mensaje = "Feliz cumpleaños Pedro!", de = "De Pedro")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(mensaje: String, de: String, modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier
+    ) {
+        Text(
+            text = mensaje,
+            fontSize = 65.sp,
+            lineHeight = 75.sp,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = de,
+            modifier = modifier
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Codelab1Theme {
+        Greeting(mensaje = "Feliz cumpleaños Pedro!", de = "De Pedro")
+    }
+}
