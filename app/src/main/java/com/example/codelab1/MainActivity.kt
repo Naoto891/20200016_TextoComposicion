6731b843f276dde1bd4b9b4347c650e069d60a1b
Naoto891 <pedromurillo0809@gmail.com>
2023-08-30 20:05:03 -0500

    Arreglado

--- app/src/main/java/com/example/codelab1/MainActivity.kt
+++ app/src/main/java/com/example/codelab1/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -47,7 +48,8 @@ fun Greeting(mensaje: String, de: String, modifier: Modifier = Modifier) {
         )
         Text(
             text = de,
-            modifier = modifier
+            fontSize = 36.sp,
+            modifier = modifier.align(Alignment.End)
         )
     }
 }
